8333a68969c8516b2a5926720a3c5bfd8787d710
Thomas Andre <thomasandre84@gmail.com>
2021-05-07 08:29:00 +0200

    init JobStoreService

--- portal-scheduler/src/main/java/org/example/scheduler/service/JobStoreService.java
+++ portal-scheduler/src/main/java/org/example/scheduler/service/JobStoreService.java
@@ -1,7 +1,25 @@
 package org.example.scheduler.service;
 
+import org.quartz.JobBuilder;
+import org.quartz.JobDataMap;
+import org.quartz.JobDetail;
+import org.quartz.JobPersistenceException;
+import org.quartz.impl.JobDetailImpl;
+import org.quartz.impl.jdbcjobstore.JobStoreCMT;
+import org.quartz.spi.JobStore;
+
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class JobStoreService {
+
+    public boolean storeSchedule() throws JobPersistenceException {
+        JobStore jobStore = new JobStoreCMT();
+
+        JobBuilder jobBuilder = new JobDetailImpl().getJobBuilder();
+        JobDataMap jobDataMap = new JobDataMap();
+        JobDetail jobDetail = jobBuilder.setJobData(jobDataMap).build();
+        jobStore.storeJob(jobDetail, true);
+        return true;
+    }
 }
